6a373dc0a8459cb784715fc3669e595cc9e28edd
FARL555 <244083735+FARL555@users.noreply.github.com>
2025-11-14 23:14:48 -0400

    Try case insensitive in constructor

--- src/playlist/playlistsortproxymodel.cpp
+++ src/playlist/playlistsortproxymodel.cpp
@@ -11,6 +11,7 @@ PlaylistSortProxyModel::PlaylistSortProxyModel(QObject *parent)
     : QSortFilterProxyModel(parent)
 {
     setDynamicSortFilter(true);
+    setSortCaseSensitivity(Qt::CaseInsensitive);
 }
 
 void PlaylistSortProxyModel::sortItems(Sort sortMode)
